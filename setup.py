083770dc6a5e205e6be9700c92c005daf9c8550b
yuvipanda <yuvipanda@gmail.com>
2018-08-11 01:05:09 -0700

    Remove install_requires for the-littlest-jupyterhub

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import setup
 
 setup(
     name="tljh-pangeo",
-    install_requires="the-littlest-jupyterhub",
     entry_points={"tljh": ["pangeo = tljh_pangeo"]},
     py_modules=["tljh_pangeo"],
 )
