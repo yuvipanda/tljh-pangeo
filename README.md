02ba4f2214513b9fc71e2c9c950392e0d2552a6f
yuvipanda <yuvipanda@gmail.com>
2018-08-11 00:57:59 -0700

    Add more packages, lab default and a README

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# tljh-pangeo
-TLJH Plugin for setting up the Pangeo Stack
+# Pangeo Stack Plugin for The Littlest JupyterHub
+
+A plugin for [The Littlest JupyterHub](https://z2jh.jupyter.org)
+that installs the [pangeo stack](http://pangeo-data.org/packages.html).
+
+
+## Installed software
+
+Packages installed:
+
+1. xarray
+2. iris
+3. dask
+
+These are installed from conda-forge.
--- tljh_pangeo.py
+++ tljh_pangeo.py
@@ -3,4 +3,18 @@ from tljh.hooks import hookimpl
 
 @hookimpl
 def tljh_extra_user_conda_packages():
-    return ['xarray']
+    # FIXME: specify versions here.
+    return [
+        'xarray',
+        'iris',
+        'dask',
+    ]
+
+
+@hookimpl
+def tljh_config_post_install(config):
+    """
+    Set JupyterLab to be default
+    """
+    if 'user_environment' not in config or 'default_app' not in config['user_environment']:
+        config['user_environment']['default_app'] = 'jupyterlab'
